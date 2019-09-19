3994e9cfd1534a1296ee16f42b87fc718b337d3d
Fabian Dean <fabian@fabiandean.dev>
2019-09-19 16:01:19 -0700

    Refactoring

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,17 @@
 
 import React from 'react';
 import Layout from '../components/Layout';
-import { searchMovieByTitle } from '../utils/API';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = props => {
     return (
         <Layout>
             <h1>Media Tracker</h1>
-            <p>Home page</p>
-            <p>{`Res: ${props.result}`}</p>
+
         </Layout>
     );
 };
 
-Home.getInitialProps = async () => {
-    const res = await searchMovieByTitle();
-    return { result: res.response }
-};
+
 
 export default Home;
