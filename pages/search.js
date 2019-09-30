8233d8e886760abeefad204d4156173f9d68510d
Fabian Flores <fabian.flores96@gmail.com>
2019-09-29 19:31:17 -0700

    Update search.js
    Page won't render certain content when there are no query params & implemented a check in getInitialProps to avoid unnecessarily querying API

--- pages/search.js
+++ pages/search.js
@@ -5,17 +5,30 @@
  * Will request search results from our server here.
  */
 
-import { searchMovieByTitle, searchShowByTitle } from '../utils/API';
 import Layout from '../components/Layout';
+import { Container, Row, Col } from 'react-bootstrap';
+import SearchBar from '../components/SearchBar';
 import { useRouter } from 'next/router';
+import { searchMovieByTitle, searchShowByTitle } from '../utils/API';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+const containerStyle = {
+    padding: 0,
+    margin: 0
+};
+
+const rowStyle = {
+    width: '100%',
+    paddingLeft: 0,
+    paddingRight: 0
+};
 
 const cardStyle = {
     backgroundColor: 'lightgray',
     borderRadius: 5,
     padding: 5,
-    margin: 0
+    margin: 5
 };
 
 const itemStyle = {
@@ -28,33 +41,52 @@ const Search = props => {
 
     return (
         <Layout>
-            <h1>Search</h1>
+            <Container style={containerStyle}>
+            <Row style={rowStyle}>
+            <Col><h1>Search</h1></Col>
+            </Row>
             <br />
-            <h5>Query</h5>
-            <p>{`${router.query.title}`}</p>
-            <h5>Results</h5>
-            {
-                results.map((item, index) => {
-                    return (
-                        <div key={index} style={cardStyle}>
-                            <p style={itemStyle}>{`${index+1}) ${item.title}`}</p>
-                        </div>
-                    );
-                })
+            <Row style={rowStyle} float='center'>
+            <Col><SearchBar /></Col> {/* ...props to implemenet child components inheriting size */}
+            </Row>
+            <br />
+            { 
+                // If no search was made, just '--domain--/search', omit search results section
+                typeof router.query.title !== 'undefined' ?
+                    <div>
+                        <h5>Results for:</h5>
+                        <p>{`${router.query.title}`}</p>
+                        {
+                            results.map((item, index) => {
+                                return (
+                                    <div key={index} style={cardStyle}>
+                                        <p style={itemStyle}><b>{index+1}</b>) {item.title}</p>
+                                        <p style={itemStyle}><i>{item.overview}</i></p>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                :
+                    null
             }
-
-            <div>
-                <p>{}</p>
-            </div>
+            </Container>
         </Layout>
     );
 };
 
 // possibly return query in return object to remove useRouter() dependency?
 Search.getInitialProps = async ({req, query: { title }}) => {
-    const res = await searchMovieByTitle(title);
+    // avoids unneccessary call to API if no query params were given
+    if (typeof title !== 'undefined') {
+        console.log('in here')
+        const res = await searchMovieByTitle(title);
 
-    return { result: res.response }
+        return { result: res.response }
+    } else {
+        console.log('it worked');
+        return { result: 'undefined'}
+    }
 };
 
 export default Search;
