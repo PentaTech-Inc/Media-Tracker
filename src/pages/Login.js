d0ed7305557933c4273385bae335f6aed7c11b66
Fabian Dean <fabian@fabiandean.dev>
2019-11-08 04:28:11 -0800

    Login page works: user authenticated after form and secret page <Profile /> is made accessible

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import { useState } from 'react';
 import { Button, FormGroup, FormControl, Form } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
 import '../styles/Login.css';
 
 const Login = props => {
@@ -14,6 +15,19 @@ const Login = props => {
 
     function handleSubmit(event) {
         event.preventDefault();
+        fetch("http://localhost:5000/api/auth?email=" + email + "&password=" + password)
+            .then(res => {
+                if (res.status === 200) {
+                    props.history.push('/');
+                } else {
+                    const error = new Error(res.error);
+                    throw error;
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                alert('Error logging in please try again');
+            });
     }
 
     return (
