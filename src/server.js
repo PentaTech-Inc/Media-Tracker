63766120a218877e9ba82a63727620326a0a8a6a
Miguel Menjivar <menjivarmiguel221@gmail.com>
2019-11-18 21:53:15 -0800

    updating location of index folder

--- src/server.js
+++ src/server.js
@@ -17,9 +17,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, './src/build')));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, './src/build/index.html'));
 });
 
 app.use(cors({
