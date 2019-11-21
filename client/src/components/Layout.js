d7348a42ab6c71240e0e5fa70465ffe484f83b5e
Miguel Menjivar <menjivarmiguel221@gmail.com>
2019-11-21 00:04:55 -0800

    updating the css for search page, which didn't work the first time so its inline css now

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -21,14 +21,30 @@ const layoutStyle = {
     minHeight: '45vh'
 };
 
+const site = {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+};
+
+const siteContent = {
+    flex: '1 0 auto',
+    padding: 'var(--space) var(--space) 0',
+    width: '100%'
+};
+
+
+
 /**
  * @param props consists of the components between <Layout> *props* </Layout> tags
  * @returns Layout component with Header and formatted page content
  */
 const Layout = props => {
     return (
-        <div className="Site">
-            <div className="Site-content">
+        // <div className="Site">
+        <div style={site}>
+            {/* <div className="Site-content"> */}
+             <div style={siteContent}>
                 <Header /> {/* will be displayed across all pages */}
                 <div style={layoutStyle}>
                     {props.children} {/* page's actual content */}
